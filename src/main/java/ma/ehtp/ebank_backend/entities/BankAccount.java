bc9cca7ab089bb871dab6970201320eaf0d6fe43
Slimani-amin <amineslimani1610@gmail.com>
2025-02-18 06:37:21 +0100

    Refactor BankAccount inheritance strategy and remove discriminator values for account types

--- src/main/java/ma/ehtp/ebank_backend/entities/BankAccount.java
+++ src/main/java/ma/ehtp/ebank_backend/entities/BankAccount.java
@@ -11,10 +11,10 @@ import lombok.NoArgsConstructor;
 import ma.ehtp.ebank_backend.enums.AccountStatus;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "TYPE", length = 4)
+@Inheritance(strategy = InheritanceType.JOINED)
+//@DiscriminatorColumn(name = "TYPE", length = 4)
 @Data @NoArgsConstructor @AllArgsConstructor
-public class BankAccount {
+public abstract class BankAccount {
     @Id
     private String id;
     private double balance;
