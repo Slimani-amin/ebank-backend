e96e23ac90993905416298d3253d388244d22aef
Slimani-amin <amineslimani1610@gmail.com>
2025-02-25 08:11:25 +0000

    Add BalanceNotSufficientException for handling insufficient balance errors

---
+++ src/main/java/ma/ehtp/ebank_backend/exceptions/BalanceNotSufficientExeption.java
@@ -0,0 +1,9 @@
+package ma.ehtp.ebank_backend.exceptions;
+
+public class BalanceNotSufficientExeption extends Exception{
+
+    BalanceNotSufficientExeption(String message){
+        super(message);
+    }
+
+}
